160b2e32bfea9e1160217114e476326aa22a07d3
finaclemence15 <finaclemence15@gmail.com>
2019-08-30 14:33:23 +0200

    creating user-description component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { UserDescriptionComponent } from './user-description/user-description.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    UserDescriptionComponent
   ],
   imports: [
     BrowserModule,
