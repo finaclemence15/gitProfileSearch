fc41e33ea3702f395524115c527f16af286d0bb0
finaclemence15 <finaclemence15@gmail.com>
2019-08-30 17:02:09 +0200

    creating githup-profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { UserDescriptionComponent } from './user-description/user-description.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    UserDescriptionComponent
+    UserDescriptionComponent,
+    GithubProfileComponent,
+    
   ],
   imports: [
     BrowserModule,
--- src/app/user-description/user-description.component.ts
+++ src/app/user-description/user-description.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+// import { profile-class } from '../';
+// import { Repo } from '../repo-class/repo';
 
 @Component({
   selector: 'app-user-description',
@@ -6,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-description.component.css']
 })
 export class UserDescriptionComponent implements OnInit {
+    // arrayRepo:Repo[];
+    @Input() repo:Repo;
 
-  constructor() { }
+
+  constructor(private userServices:userRequestServices) { }
 
   ngOnInit() {
   }
